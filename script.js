e35d6f44888a5b85c0fe1562b65aab6984ab46cd
neerajarakkan <ashiashlin24@gmail.com>
2025-09-24 10:23:37 +0530

    Updated initiative link and CSS

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
     window.addEventListener('load', () => {
-      // Slide down header
       document.getElementById('header').classList.add('slide-down');
 
-      // Slide up main content
       setTimeout(() => {
         document.getElementById('mainContent').classList.add('slide-up');
       }, 100);
